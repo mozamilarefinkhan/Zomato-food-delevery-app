1fe32f9d657ab90a5729f467bb22f4095db2ad24
Mozamil Arefin Khan <mozamilkhan43@gmail.com>
2023-01-23 22:32:46 +0530

    first commit

---
+++ src/components/CardsData.js
@@ -0,0 +1,112 @@
+const Cardsdata = [
+    {
+        id: 1,
+        rname: "Masala Theory",
+        imgdata: "https://th.bing.com/th/id/OIP.RsjfACSqDr6ur-T09PvJHgHaE7?w=228&h=180&c=7&r=0&o=5&dpr=1.5&pid=1.7",
+        address: "North Indian, Chicken Dum Biryani, Mughlai, Veg Biryani",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: " 1175 + order placed from here recently",
+        price: 250,
+        rating: "4.8",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+        qnty:0
+    },
+    {
+        id: 2,
+        rname: "Chinese Food",
+        imgdata: "https://th.bing.com/th/id/OIP.kldWnCGzv_R5Pnp8txQmVgHaE8?pid=ImgDet&rs=1",
+        address: "Quick Noodles, Spring Rolls, Cantonese Chicken Soup, Stir-fried Tofu with Rice",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: " 2525 + order placed from here recently",
+        price: 300,
+        rating: "3.9",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+         qnty:0
+    },
+    {
+        id: 3,
+        rname: "La Milano Pizza",
+        imgdata: "https://th.bing.com/th/id/OIP.1uG7WoiNMkggjK0ZMh9ebwHaF7?w=227&h=182&c=7&r=0&o=5&dpr=1.5&pid=1.7",
+        address: "Pizza, Fast Food, Pasta",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: " 650 + order placed from here recently",
+        price: 650,
+        rating: "4.2",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+         qnty:0
+    },
+    {
+        id: 4,
+        rname: "Wow Momo",
+        imgdata: "https://www.swantour.com/blogs/wp-content/uploads/2019/05/Momos-768x547.jpg",
+        address: "Steamed Momos, Fried Momos, Tandoori Momos, Chocolate Momos",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: " 300 + order placed from here recently",
+        price: 150,
+        rating: "3.8",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+         qnty:0
+    },
+    {
+        id: 5,
+        rname: "Jassi De Parathe",
+        imgdata: "https://b.zmtcdn.com/data/pictures/chains/5/110225/3978e28854b7496dbef9496546734811_o2_featured_v2.jpg",
+        address: "North Indian",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: "1050 + order placed from here recently",
+        price: 210,
+        rating: "4.0",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+         qnty:0
+    },
+    {
+        id: 6,
+        rname: "Anjoy Latenight",
+        imgdata: "https://th.bing.com/th/id/OIP.8N37UQfNGj0ex3KTNDMPxwHaF6?pid=ImgDet&rs=1",
+        address: "Milk Shake, Strawberry Shake, Oreo Shake, Mocktaile",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: " 1100 + order placed from here recently",
+        price: 200,
+        rating: "3.8",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+         qnty:0
+    },
+    {
+        id: 7,
+        rname: " Baskin Robbins",
+        imgdata: "https://images.wisegeek.com/seven-scoops-of-ice-cream-with-berries.jpg",
+        address: "Vanila, Strawberry, Chocolate, Butter Sctoch, Mango ",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: "500 + order placed from here recently",
+        price: 300,
+        rating: "3.8",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+         qnty:0
+    },
+    {
+        id: 8,
+        rname: "Red Rooster Cafe",
+        imgdata: "https://image.makewebeasy.net/makeweb/0/K5HN2G4Kq/DefaultData/tea_coffee.jpg",
+        address: "Tea, Coffee, Green Tea, Cappuccino Coffee",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: "500 + order placed from here recently",
+        price: 100,
+        rating: "3.2",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+         qnty:0
+    },
+    {
+        id: 9,
+        rname: "Burger King",
+        imgdata: "https://th.bing.com/th/id/OIP.WOH7M74afOlrvjJ-wj-ZZAHaE8?pid=ImgDet&rs=1",
+        address: "Veg Burger, Chicken Burger, Flame Grilled Burger, Classic fries",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: "2525 + order placed from here recently",
+        price: 550,
+        rating: "3.8",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+         qnty:0
+    },
+];
+
+export default Cardsdata;
